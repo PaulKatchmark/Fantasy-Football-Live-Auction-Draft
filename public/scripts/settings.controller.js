80271e0634d9e797cd2d174dafbb6cfed6fd57b0
PaulKatchmark <paulkatchmark@gmail.com>
2016-11-08 10:36:14 -0600

    set up page complete

--- public/scripts/settings.controller.js
+++ public/scripts/settings.controller.js
@@ -1,7 +1,5 @@
 angular.module('auctionApp')
- .controller('SettingsController', SettingsController);
-
- function SettingsController($http, $location) {
+ .controller('SettingsController', ['$location', 'DataService', function ($location, DataService) {
    console.log('SettingsController loaded');
    var vm = this;
    vm.numTeams = [6,7,8,9,10,11,12,13,14,15,16,17,18];
@@ -10,12 +8,26 @@ angular.module('auctionApp')
    vm.runningBacks = [2,3];
    vm.wideReceivers = [2,3];
    vm.tightEnds = [1,2];
-   vm.flexSpot = [0,1];
-   vm.kicker = [1];
-   vm.defense = [1];
+   vm.flexSpot = [0,1,2];
+   vm.kicker = [0,1];
+   vm.defense = [0,1];
    vm.benchSpots = [2,3,4,5,6,7,8];
-   vm.totalTeams = [];
+   vm.setTeams = [0,1,2,3,4,5,6,7,8];
    vm.teamsDone = false;
+   vm.setQB = 0;
+   vm.setLeague = false;
+   vm.teamSet = false;
+
+   vm.team = {
+     quarterBacks: 0,
+     runningBacks: 0,
+     wideReceivers: 0,
+     tightEnds: 0,
+     flexSpot: 0,
+     kicker: 0,
+     defense: 0,
+     benchSpots: 0
+   }
 
    vm.setUpLeague = {
     model: null,
@@ -25,22 +37,31 @@ angular.module('auctionApp')
     ]
   };
 
-  vm.addTeams = function (){
-    if (vm.newTeams>0) {
-      for (i = 0; i < vm.newTeams; i++ ){
-        vm.totalTeams.push("team" + (i+1));
-        console.log(totalTeams);
-      }
-      vm.teamsDone = true;
-    }
-  }
+  vm.hideShow = function() {
+    vm.setLeague = true;
+    console.log('vm.setLeague ',vm.setLeague);
+    vm.teamSet = true;
+  };
+  vm.createTeamSize = function (team){
+    console.log('setQB ', vm.setQB);
+    console.log('quarterBacks ', vm.quarterBacks);
+    console.log('setRB ', vm.setRB);
+  };
 
-vm.searchAndDisplayValues = function (){
-  if(vm.totalTeams.length === 0){
-    vm.addTeams();
-  }
-}
+// vm.addTeams = function (){
+//   if (vm.newTeams>0) {
+//     for (i = 0; i < vm.newTeams; i++ ){
+//       vm.totalTeams.push("team" + (i+1));
+//       console.log(totalTeams);
+//     }
+//     vm.teamsDone = true;
+//   }
+// }
 
+// vm.searchAndDisplayValues = function (){
+//   if(vm.totalTeams.length === 0){
+//     vm.addTeams();
+//   }
+// }
 
-
-};
+}]);
--- public/scripts/setup.service.js
+++ public/scripts/setup.service.js
@@ -1,36 +1,19 @@
-// app.service('auctionApi', setUpService);
+angular.module('auctionApp')
+.service('DataService',['$http', function($http){
+  // var data = {};
+  // var setTeams = [];
+  // console.log('inside setUpServie', setQB);
+  //
+  //
+  // teamSize = function () {
+  //   console.log('test service', setQB);
+  // }
+  // return $http.post('/positions', )
 
-// angular.module('auctionApp').factory('DataService',['$http', function($http){
 
 
-// function getPlayers(){
-//   $http.get('/players').then(handleSuccess, handleFailure);
-// }
-// function handleSuccess(res){
-//     data.players = res.data
-//   }
-//   //generic failure request function
-//   function handleFailure(res){
-//     console.log('/players fail', res);
-//   }
-//
-// return {
-//   getPlayers: getPlayers,
-// }
-// }]);
 
-// function setUpService($http) {
-//   var API = 'http://www.fantasyfootballnerd.com/service/';
-//   var API_KEY =
-//   var format = 'service/auction/json/';
-//   var ppr = '1';
-//   var standard = '0';
-//
-//   return {
-//
-//     auctionValueInfo: function (keywords) {
-//       // var separatedValues = keywords.replace(' ', '+');
-//       return $http.get('http://www.fantasyfootballnerd.com/service/auction/json/test/ppr/');
-//     },
-//   };
-// }
+
+
+
+}]);
